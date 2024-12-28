26e7db6e0708d2f42863b200f07f5bbe418792f1
Iresh Eranga <iresheranga2020al@gmail.com>
2024-12-28 23:18:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:first_app/screens/floating_action_btn.dart';
+import 'package:first_app/screens/stack_positioned.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Flutter App',
       themeMode: ThemeMode.system,
-      home: FloatingActionBtn()
+      home: StackPositioned()
     );
   }
 }
