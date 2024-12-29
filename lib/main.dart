2f31ec433ba86346c4b4a64e66b08e25f86e7316
Iresh Eranga <iresheranga2020al@gmail.com>
2024-12-29 20:03:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:first_app/screens/custom_widgets.dart';
-import 'package:first_app/screens/floating_action_btn.dart';
-import 'package:first_app/screens/stack_positioned.dart';
 import 'package:flutter/material.dart';
 
 void main() {
